3518a119125e65728adabc9995805c4f135b8018
makhtar18 <mehvishakhtar18@gmail.com>
2023-06-05 10:54:06 -0700

    Added files for railway deployment

--- UserDetails/settings.py
+++ UserDetails/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-allce)k!*2pkkl##&+!2$@zhi=#9l+3d+c)9jz^gp(z*9x4e#4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -135,6 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
